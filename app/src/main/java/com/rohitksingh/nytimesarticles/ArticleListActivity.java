0d18a8e905d0cf89d5b17fb08f09e44b19fedb56
Rohit <r4rohit002@gmail.com>
2021-05-28 13:29:26 -0700

    Added searchView in the Activity

--- app/src/main/java/com/rohitksingh/nytimesarticles/ArticleListActivity.java
+++ app/src/main/java/com/rohitksingh/nytimesarticles/ArticleListActivity.java
@@ -2,23 +2,14 @@ package com.rohitksingh.nytimesarticles;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
+public class ArticleListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_article_list);
     }
 
 }
