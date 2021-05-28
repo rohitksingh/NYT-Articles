bffa55dc924a6c49fa30738b700f255bbd348f75
Rohit <r4rohit002@gmail.com>
2021-05-28 14:18:53 -0700

    Created Repository and ViewModels

--- app/src/main/java/ui/ArticleListActivity.java
+++ app/src/main/java/ui/ArticleListActivity.java
@@ -1,20 +1,29 @@
-package com.rohitksingh.nytimesarticles;
+package ui;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.rohitksingh.nytimesarticles.R;
+import com.rohitksingh.nytimesarticles.databinding.ActivityArticleListBinding;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.databinding.DataBindingUtil;
 
 public class ArticleListActivity extends AppCompatActivity{
 
     private SearchView searchView;
     private TextView searchTitle;
 
+    private ActivityArticleListBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initDataBinding();
+
         setContentView(R.layout.activity_article_list);
 
         searchView = findViewById(R.id.searchView);
@@ -35,6 +44,10 @@ public class ArticleListActivity extends AppCompatActivity{
             }
         });
 
+    }
+
+    private void initDataBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_article_list);
     }
 
 }
