058ee3710785cdd96c763b9c3c4ace68a648a298
Rohit <r4rohit002@gmail.com>
2021-05-29 14:36:01 -0700

    Integrated Suggested Article API results with SeggestedRecyclerView

--- app/src/main/java/viewmodels/ArticleListViewModel.java
+++ app/src/main/java/viewmodels/ArticleListViewModel.java
@@ -20,9 +20,9 @@ public class ArticleListViewModel extends AndroidViewModel {
 
     public ArticleListViewModel(@NonNull Application application) {
         super(application);
-        suggestionListLiveData = new MutableLiveData<>(new ArrayList<>());
         articleRepository = ArticleRepository.getInstance();
         articleListLiveData = articleRepository.getArticleLiveData();
+        suggestionListLiveData = articleRepository.getSuggestedArticles();
     }
 
     public LiveData<List<Article>> getArticlesLiveData(){
@@ -37,34 +37,18 @@ public class ArticleListViewModel extends AndroidViewModel {
         suggestionListLiveData.setValue(new ArrayList<>());
     }
 
+    //To fetch history 10 Items
     public void loadSuggestionsFromRoom(){
-        List<Article> suggestions = articleRepository.getSuggestedArticles();
-        suggestionListLiveData.postValue(suggestions);
+        //List<Article> suggestions = articleRepository.getSuggestedArticles();
+        //suggestionListLiveData.postValue(suggestions);
     }
 
     public void loadArticlesFromAPI(String searchTerm){
         articleRepository.fetchArticlesFromAPI(searchTerm);
+    }
 
-//        articleListLiveData.postValue(articles);
-
-//        Thread t = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for(int i=0;i<6;i++){
-//                    try {
-//                        Thread.sleep(1000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//
-//                articleListLiveData.postValue(articles);
-//            }
-//        });
-
-//        t.start();
-
-
+    public void getSuggestedArticles(String keywords){
+        articleRepository.fetchSuggestedArticlesFromAPI(keywords);
     }
 
 
